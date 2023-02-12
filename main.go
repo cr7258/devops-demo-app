4bcba283d78d58f4a35c45086cd04db19b9fd254
Cheng, Seven <seven.cheng@sap.com>
2023-02-12 15:59:24 +0800

    HTTP Server

---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"log"
+	"net/http"
+	"os"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		environment := os.Getenv("Environment")
+		w.Write([]byte("hello devops: " + environment))
+	})
+	log.Print("Server started, Listening on port 10000...")
+	err := http.ListenAndServe(":10000", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
